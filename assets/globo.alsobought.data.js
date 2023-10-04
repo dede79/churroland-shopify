46982443365319b15fa5d3221b445223ea3e8be8
andreahurren <andrea.hurren@gohomedirect.com>
2023-10-04 22:18:31 +0100

    transferring from local files

---
+++ assets/globo.alsobought.data.js
@@ -0,0 +1,5 @@
+globoRelatedProductsConfig.manualRecommendations = {"1956248780870":["1956247896134","1956248584262","1956248748102","1956248584262"]};
+globoRelatedProductsConfig.manualVendors = {};
+globoRelatedProductsConfig.manualTags = {};
+globoRelatedProductsConfig.manualProductTypes = {};
+globoRelatedProductsConfig.manualCollections = {};
